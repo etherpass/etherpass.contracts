bf190aa5271c6986f79fe2c2d4d3a9e35c3c866d
Roman Chuprikov <paradise.develop@gmail.com>
2017-07-06 22:51:38 +0500

    getKeys

--- contracts/etherpass_main.sol
+++ contracts/etherpass_main.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.1;
-import 'zeppelin/ownership/Ownable.sol';
+import '../installed_contracts/zeppelin/contracts/ownership/Ownable.sol';
 
 
 contract Etherpass is Ownable {
@@ -11,10 +11,7 @@ contract Etherpass is Ownable {
 
     mapping (address => mapping(bytes32 => bytes)) private _storage;
 
-    mapping (address => KeysGroup) private _indexes;
-
-    //KeysGroup[] keyGroups = new KeysGroup[1];
-
+    mapping (address => KeysGroup) private _keyGroups;
 
     function getPassword(bytes key, bytes sign) returns (bytes) {
         bytes32 hash = sha3(key); 
@@ -25,64 +22,32 @@ contract Etherpass is Ownable {
     function setPassword(bytes key, bytes value, bytes sign){
         bytes32 hash = sha3(key, value); 
         address owner = _getAddress(hash, sign);
-        _storage[owner][sha3(key)] = value;
-
-        var keyGroup = _indexes[owner];
-
-        keyGroup.keys[keyGroup.numKeys] = key;
-        keyGroup.numKeys++;
-
-        /*uint index = _indexes[owner];
-        if (index == 0)
+        bytes32 keyHash = sha3(key);
+        if (_storage[owner][keyHash].length == 0)
         {
-            index = keyGroups.length;
-            keyGroups.length++;            
-            keyGroups[index] = KeysGroup();              
+            _keyGroups[owner].keys[_keyGroups[owner].numKeys] = key;
+            _keyGroups[owner].numKeys++;
         }
-        bool found = false;
-        var map = keyGroups[index].keys;
-          for(uint i=0;i<keyGroups[index].numKeys;i++)
-            if (equalsArrays(map[i], key))
-            {
-                found = true;
-                break;
-            }
-        if (!found)
-        {            
-            keyGroups[index].numKeys ++;
-        }
-        bytes keys = _keys[owner];
-        bool setted = false;
-        for(uint i=0;i<keys.length;i++)
-            if (equalsArrays(keys[i], key))
-            {
-                keys[i] = key;
-                setted = true;
-                break;
-            }
-        if (!setted)
-        {
-            keys.length = keys.length+1;
-            keys[keys.length-1] = key;
-        }
-        _keys[owner] = ref keys;*/
+        _storage[owner][keyHash] = value;
     }
 
     function getKeys(address addr) returns (bytes) {
-
-        bytes storage result = new bytes(1);
-        uint j = 0;
-        for(uint i=0;i<_indexes[addr].numKeys;i++)
+        uint i = 0;
+        uint totalSize = 0;
+        for(i=0;i<_keyGroups[addr].numKeys;i++)
         {
-            bytes key = _indexes[addr].keys[i];
-            uint keyLengthIndex = j;
-            j++;
-            for(uint k=0;k< key.length;k++)
-                {
-                    result[keyLengthIndex] =  byte(uint(result[keyLengthIndex]) + 1);
-                    result[j++] = key[k];
-                    result.length = result.length + 1;
-                }                                         
+            totalSize += _keyGroups[addr].keys[i].length + 1;
+        }
+
+        bytes memory result = new bytes(totalSize);
+
+        uint index = 0;
+        for(i=0;i<_keyGroups[addr].numKeys;i++)
+        {
+            bytes key = _keyGroups[addr].keys[i];
+            result[index++] = byte(key.length);
+            for(uint j=0; j<key.length; j++)
+                result[index++] = key[j];
         }
         return result;
     }
--- test/etherpass_main.js
+++ test/etherpass_main.js
@@ -4,13 +4,18 @@ const ethUtil = require('ethereumjs-util');
 
 contract('Etherpass', function(accounts) {
 
-    var private_key = "4085dde01ea641a0f4fd6586ca11fc1f5df38e1bdcbef501da970cad9335b389"
+    var private_key = "0x4085dde01ea641a0f4fd6586ca11fc1f5df38e1bdcbef501da970cad9335b389";
+    var address = "0x960336a077fb32d675405bd0a6cd0cb74aaa5062";
     
     it("test set and get password", function(){
-        var url = "0x1200";
-        var password = "0x0123"                
+        var url = "0x1234";
+        var url2 = "0x9999";
+        var password = "0x0123";
 
         var setSign = signer.signHash(signer.getHash(url, password), private_key);
+
+        var setSign2 = signer.signHash(signer.getHash(url2, password), private_key);
+
         var getSign = signer.signHash(signer.getHash(url), private_key);                        
 
         var etherpass = null;
@@ -18,6 +23,13 @@ contract('Etherpass', function(accounts) {
         .then(function(instance){           
             etherpass = instance;
             return etherpass.setPassword(url, password, setSign);
+        }).then(function(){
+            return etherpass.setPassword(url2, password, setSign2);            
+        })
+        .then(function(){
+            return etherpass.getKeys.call(address).then(function(data) {
+                console.log(data);
+            });            
         })
         .then(function(){
             return etherpass.getPassword.call(url, getSign);            
